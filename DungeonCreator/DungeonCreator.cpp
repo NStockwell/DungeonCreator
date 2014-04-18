7854cbc8fbe98cd682e97beec7df9e084b2faef3
Nuno Moreira <nscarnaut@gmail.com>
2014-04-18 16:04:40 +0100

    creation of the dungeon creatorr proj

---
+++ DungeonCreator/DungeonCreator.cpp
@@ -0,0 +1,25 @@
+// DungeonCreator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void print();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	while(1)
+	{
+		print();
+	}
+	return 0;
+}
+
+
+void print()
+{
+	cout << "Hello World";
+}
